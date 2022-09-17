ffe9b98f0d8af4649a5b0c63ae5cf03190e0716d
Juan <juancruzardanaz@gmail.com>
2022-09-17 17:38:08 -0300

    desktop breakpoints adjustment

---
+++ assets/script/main.js
@@ -0,0 +1,9 @@
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true,
+});
+
