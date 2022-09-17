6a7bff67bfd9481285c3bbbdc5db49d7f8e76bc9
Juan <juancruzardanaz@gmail.com>
2022-09-17 18:30:11 -0300

    scroll reveal library added

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,3 +7,12 @@ const sr = ScrollReveal({
     // reset: true,
 });
 
+sr.reveal(`.primary-container-data`);
+sr.reveal(`.primary-button-container`, {delay: 400, origin: 'bottom'});
+sr.reveal(`.destination-img`, {delay: 600});
+sr.reveal(`.destination-subheading`, {delay: 400, origin: 'left'});
+sr.reveal(`.destination-data-container`, {delay: 600, origin: 'bottom'});
+sr.reveal(`.crew-subheading`, {delay: 400, origin: 'left'});
+sr.reveal(`.crew-container`, {delay: 400, origin: 'bottom'});
+sr.reveal(`.technology-subheading`, {delay: 400, origin: 'left'});
+sr.reveal(`.technology-container`, {delay: 400, origin: 'bottom'});
